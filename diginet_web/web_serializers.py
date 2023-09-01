03f74ecb22d11a939938b56e7db2365e2af32f0e
Alex Poma <alex.rpoma@outlook.com>
2023-09-01 11:59:10 -0400

    web serializers

---
+++ diginet_web/web_serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Customer, Address
+
+class CustomerSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Customer
+    fields = ('uuid', 'username', 'fullname', 'email', 'address', 'create_at')
+    read_only_fields = ('created_at')
+
+class AddressSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Address
+    fields = ('uuid', 'street', 'city', 'country')
